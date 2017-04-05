23fbf6e25e0d005f4ddd2d0bb52b027eb2243d44
Tom Theisen <github@tomtheisen.com>
2017-04-04 22:10:28 -0700

    backup save

--- game/game.ts
+++ game/game.ts
@@ -2,33 +2,41 @@ import GameState from "gamestate";
 import * as Machine from "machine";
 
 const saveKey = "mp_save";
+const backupKey = "mp_backup"
+const tickTime = 500;
+const autoSaveTime = 60000;
 
 let state = new GameState;
 
-function save() {
-    localStorage.setItem(saveKey, /* btoa */(JSON.stringify(state.serialize())));
+function save(key: string) {
+    localStorage.setItem(key, /* btoa */(JSON.stringify(state.serialize())));
 }
 
-function load() {
-    let saved = localStorage.getItem(saveKey);
+function load(key: string) {
+    let saved = localStorage.getItem(key);
     if (saved) {
         state.deserialize(JSON.parse( /* atob */ (saved)));
         state.tick();
-    } else {
-        state.reset();
     }
 }
 
 // debug mode - free
 Object.keys(Machine.allMachines).forEach(k => Machine.allMachines[k].basePrice = 0);
 
-document.getElementById("save-button")!.addEventListener("click", save);
-document.getElementById("load-button")!.addEventListener("click", load);
-document.getElementById("reset-button")!.addEventListener("click", ev => state.reset() );
+function clickHandler(id: string, handler: EventListener) {
+    let el = document.getElementById(id);
+    if (el) el.addEventListener("click", handler);
+}
 
-setInterval(() => state.tick(), 500);
+clickHandler("save-button", () => save(saveKey));
+clickHandler("load-button", () => load(saveKey));
+clickHandler("backup-save-button", () => save(backupKey));
+clickHandler("backup-load-button", () => load(backupKey));
+clickHandler("reset-button", () => state.reset());
 
-load();
+setInterval(() => state.tick(), tickTime);
 
-window.addEventListener("beforeunload", save);
-setInterval(save, 60000);
+load(saveKey);
+
+window.addEventListener("beforeunload", () => save(saveKey));
+setInterval(save, autoSaveTime);
